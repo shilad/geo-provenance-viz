c6988fdc8a3ca20d0e1383d947e84ee607616e54
shilad <shilad@gmail.com>
2015-04-04 21:25:32 -0500

    Parallelizing.

--- wikibrain/src/main/java/org/macalester/geoprovenance/ExportPageViews.java
+++ wikibrain/src/main/java/org/macalester/geoprovenance/ExportPageViews.java
@@ -16,8 +16,7 @@ import org.wikibrain.core.model.LocalPage;
 import org.wikibrain.pageview.PageViewDao;
 import org.wikibrain.spatial.dao.SpatialDataDao;
 import org.wikibrain.spatial.util.ContainmentIndex;
-import org.wikibrain.utils.Scoreboard;
-import org.wikibrain.utils.WpIOUtils;
+import org.wikibrain.utils.*;
 import org.wikibrain.wikidata.WikidataDao;
 import org.wikibrain.wikidata.WikidataFilter;
 import org.wikibrain.wikidata.WikidataStatement;
@@ -27,6 +26,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 /**
@@ -52,22 +52,22 @@ public class ExportPageViews {
 
 
     public static void main(String args[]) throws ConfigurationException, DaoException, WikiBrainException, IOException {
-        Env env = EnvBuilder.envFromArgs(args);
+        final Env env = EnvBuilder.envFromArgs(args);
         LocalPageDao pageDao = env.getConfigurator().get(LocalPageDao.class);
-        PageViewDao viewDao = env.getConfigurator().get(PageViewDao.class);
+        final PageViewDao viewDao = env.getConfigurator().get(PageViewDao.class);
         WikidataDao wikidataDao = env.getConfigurator().get(WikidataDao.class);
 
         SpatialDataDao spatialDao = env.getConfigurator().get(SpatialDataDao.class);
 
         // Get country codes
-        Map<Integer, String> countryCodes = new HashMap<Integer, String>(MISSING_COUNTRY_CODES);
+        final Map<Integer, String> countryCodes = new HashMap<Integer, String>(MISSING_COUNTRY_CODES);
         for (WikidataStatement stm : wikidataDao.get(new WikidataFilter.Builder().withPropertyId(297).build())) {
             countryCodes.put(stm.getItem().getId(), stm.getValue().getStringValue());
         }
         System.err.println("found " + countryCodes.size() + " country codes");
 
         // Index countries
-        ContainmentIndex countryIndex = new ContainmentIndex();
+        final ContainmentIndex countryIndex = new ContainmentIndex();
         countryIndex.setBufferWidths(new double[] { 0.0 }); // be stingy in buffers
         Map<Integer, Geometry> countries = spatialDao.getAllGeometriesInLayer("country");
         for (int conceptId : countries.keySet()) {
@@ -79,50 +79,50 @@ public class ExportPageViews {
         }
 
         // Build leaderboard datastructures for all lang / country pairs.
-        Map<String, Scoreboard<PageViews>> top = new HashMap<String, Scoreboard<PageViews>>();
-        Map<String, Long> totals = new HashMap<String, Long>();
+        final Map<String, Scoreboard<PageViews>> top = new HashMap<String, Scoreboard<PageViews>>();
+        final Map<String, AtomicLong> totals = new HashMap<String, AtomicLong>();
         for (Language lang : env.getLanguages()) {
             for (String countryCode : countryCodes.values()) {
                 String key = lang.getLangCode() + " " + countryCode;
                 top.put(key, new Scoreboard<PageViews>(100, Scoreboard.Order.DECREASING));
-                totals.put(key, 0l);
+                totals.put(key, new AtomicLong());
             }
         }
         System.err.println("initialized leaderboard for " + top.size() + " entries");
 
         // Get mapping from universal to local ids in each language.
-        Map<Language, TIntIntMap> univToLocal = env
+        final Map<Language, TIntIntMap> univToLocal = env
                 .getConfigurator().get(UniversalPageDao.class)
                 .getAllUnivToLocalIdsMap(env.getLanguages());
         System.err.println("retrieved mapping for " + univToLocal.size() + " languages");
 
         // For each spatial concept....
-        Map<Integer, Geometry> geometries = spatialDao.getAllGeometriesInLayer("wikidata");
+        final Map<Integer, Geometry> geometries = spatialDao.getAllGeometriesInLayer("wikidata");
         System.err.println("retrieved " + geometries.size() + " geometries");
 
-        int n = 0;
-        for (int conceptId : geometries.keySet()) {
-            if (++n % 100 == 0) {
-                System.err.println("doing " + n + " of " + geometries.size());
-            }
+        ParallelForEach.loop(geometries.keySet(), new Procedure<Integer>() {
+            public void call(Integer conceptId) throws Exception {
+                // For each containing country
+                for (ContainmentIndex.Result container : countryIndex.getContainer(geometries.get(conceptId))) {
+                    String countryCode = countryCodes.get(container.id);
+                    if (countryCode == null) continue;
 
-            // For each containing country
-            for (ContainmentIndex.Result container : countryIndex.getContainer(geometries.get(conceptId))) {
-                String countryCode = countryCodes.get(container.id);
-                if (countryCode == null) continue;
-
-                // For each language that has an article in the concept
-                for (Language lang : env.getLanguages()) {
-                    if (univToLocal.containsKey(lang) && univToLocal.get(lang).containsKey(conceptId)) {
-                        int localId = univToLocal.get(lang).get(conceptId);
-                        int views = viewDao.getNumViews(lang, localId, new DateTime(0), DateTime.now());
-                        String key = lang.getLangCode() + " " + countryCode;
-                        top.get(key).add(new PageViews(lang, localId, views), views);
-                        totals.put(key, totals.get(key) + views);
+                    // For each language that has an article in the concept
+                    for (Language lang : env.getLanguages()) {
+                        if (univToLocal.containsKey(lang) && univToLocal.get(lang).containsKey(conceptId)) {
+                            int localId = univToLocal.get(lang).get(conceptId);
+                            int views = viewDao.getNumViews(lang, localId, new DateTime(0), DateTime.now());
+                            String key = lang.getLangCode() + " " + countryCode;
+                            Scoreboard<PageViews> board = top.get(key);
+                            synchronized (board) {
+                                board.add(new PageViews(lang, localId, views), views);
+                            }
+                            totals.get(key).addAndGet(views);
+                        }
                     }
                 }
             }
-        }
+        }, 1000);
 
 
         // Output the tops!
@@ -141,7 +141,7 @@ public class ExportPageViews {
                         page.getTitle(), page.getTitle().toUrl()));
                 topCounts += pv.views;
             }
-            long otherCounts = totals.get(key) - topCounts;
+            long otherCounts = totals.get(key).get() - topCounts;
             writer.write(String.format("%s\t%s\t%d\t%s\t%s\n",
                     lang.getLangCode(), countryCode, otherCounts,
                     "Other", ""));
